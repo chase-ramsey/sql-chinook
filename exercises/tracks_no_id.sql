eae3b1a82645871c25435f64091caa2d58553507
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-15 14:58:54 -0500

    Added media type to query in previous exercise

--- exercises/tracks_no_id.sql
+++ exercises/tracks_no_id.sql
@@ -3,7 +3,9 @@
 SELECT
   t.Name,
   b.Title,
+  m.Name,
   g.Name
-FROM Track t, Album b, Genre g
+FROM Track t, Album b, MediaType m, Genre g
 WHERE t.AlbumId = b.AlbumId
+AND t.MediaTypeId = m.MediaTypeId
 AND t.GenreId = g.GenreId
